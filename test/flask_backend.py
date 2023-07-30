3924b1534bbafd87d3407be8cc5054f4bf25d5e1
zhongyizh <zhongyi.zhao@tufts.edu>
2023-07-29 20:37:12 -0400

    update backend api

--- test/flask_backend.py
+++ test/flask_backend.py
@@ -1,20 +1,25 @@
-from datetime import datetime
-from flask import Flask, request, jsonify, send_from_directory, url_for
 import os
 import uuid
-from run import app
-from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
-from wxcloudrun.model import Counters
-from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
-
 import requests
+from flask import Flask, request, jsonify, send_from_directory, url_for
 from config import APP_ID, APP_SECRET, MONGODB_URI
+
+app = Flask(__name__)
+
+# Connect to MongoDB
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 # Create a new client and connect to the server
 client = MongoClient(MONGODB_URI, server_api=ServerApi('1'))
 db = client['mydatabase']
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 # create a directory for storing uploaded photos
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
@@ -29,67 +34,6 @@ if not os.path.exists(AVATAR_FOLDER):
 # Define the collections
 uploads_collection = db['uploads']
 users_collection = db['users']
-
-
-@app.route('/')
-def index():
-    """
-    :return: 返回index页面
-    """
-    return render_template('index.html')
-
-
-@app.route('/api/count', methods=['POST'])
-def count():
-    """
-    :return:计数结果/清除结果
-    """
-
-    # 获取请求体参数
-    params = request.get_json()
-
-    # 检查action参数
-    if 'action' not in params:
-        return make_err_response('缺少action参数')
-
-    # 按照不同的action的值，进行不同的操作
-    action = params['action']
-
-    # 执行自增操作
-    if action == 'inc':
-        counter = query_counterbyid(1)
-        if counter is None:
-            counter = Counters()
-            counter.id = 1
-            counter.count = 1
-            counter.created_at = datetime.now()
-            counter.updated_at = datetime.now()
-            insert_counter(counter)
-        else:
-            counter.id = 1
-            counter.count += 1
-            counter.updated_at = datetime.now()
-            update_counterbyid(counter)
-        return make_succ_response(counter.count)
-
-    # 执行清0操作
-    elif action == 'clear':
-        delete_counterbyid(1)
-        return make_succ_empty_response()
-
-    # action参数错误
-    else:
-        return make_err_response('action参数错误')
-
-
-@app.route('/api/count', methods=['GET'])
-def get_count():
-    """
-    :return: 计数的值
-    """
-    counter = Counters.query.filter(Counters.id == 1).first()
-    return make_succ_response(0) if counter is None else make_succ_response(counter.count)
-
 
 # Example endpoint to handle the login request
 @app.route('/api/login', methods=['POST'])
@@ -280,3 +224,7 @@ def download(filename):
 @app.route('/api/avatar/<path:filename>', methods=['GET'])
 def download_avatar(filename):
     return send_from_directory(AVATAR_FOLDER, filename)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
